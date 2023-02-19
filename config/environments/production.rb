9c41f87a1a4cd9ba395b3990a6429ad0f911d71e
Mark Swardstrom <swards@gmail.com>
2023-02-18 19:17:35 -0800

    Trying build packs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,10 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
+  #
+  #
+  # Image Processing on Heroku
+  config.active_storage.variant_processor = :vips
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
