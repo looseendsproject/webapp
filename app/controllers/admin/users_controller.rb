6cc56af823673f42c5d0d2d850102f19a5e83587
Mark Swardstrom <swards@gmail.com>
2023-02-24 19:19:57 -0800

    doing controller:

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -13,5 +13,14 @@ class Admin::UsersController < Admin::AdminController
 
   def update
     @user = User.find(params[:id])
+    @user.update!(user_params)
+    redirect_to [:admin, @user]
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:role)
+  end
+
 end
