692403832727cf3583be6d659e3cea4c2ba1451d
Mark Swardstrom <swards@gmail.com>
2023-02-27 22:53:12 -0800

    Example

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'example.com',
+    domain:               'looseendsproject.org',
     user_name:            Rails.application.credentials.dig(:email, :user),
     password:             Rails.application.credentials.dig(:email, :password),
     authentication:       'plain',
