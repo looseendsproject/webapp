1b9153f52b453b593b56dce1149bfa9aef3404d4
Mark Swardstrom <swards@gmail.com>
2023-02-27 22:45:03 -0800

    Now with host:

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { :host => "https://www.looseendsproject.org/" }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
