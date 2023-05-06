1e9a00a53593a5f313856fb164fb4241b31011fa
Mark Swardstrom <swards@gmail.com>
2023-05-06 00:10:40 -0700

    Manager

--- app/controllers/manage/manage_controller.rb
+++ app/controllers/manage/manage_controller.rb
@@ -3,6 +3,8 @@ class Manage::ManageController < ApplicationController
   before_action :authenticate_user!
   before_action :require_manager
 
+  layout 'manage'
+
   def require_manager
     if !current_user.can_manage?
       redirect_to root_path
