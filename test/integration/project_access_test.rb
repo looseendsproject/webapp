9093f98212a72be03713dea34f0fceeaef8d48c9
Mark Swardstrom <swards@gmail.com>
2023-07-06 20:42:37 -0700

    Adding integration test for project page

---
+++ test/integration/project_access_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class ProjectAccessTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    @one = projects :one
+    @two = projects :two
+  end
+
+  test 'project page logged out' do
+    get '/projects/1'
+    assert_redirected_to '/'
+  end
+
+  test 'project page logged in' do
+    sign_in users(:bob)
+
+    get '/projects/1'
+    assert_response :success
+  end
+end
