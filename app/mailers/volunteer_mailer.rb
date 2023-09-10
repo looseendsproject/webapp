1e9bfbd6d2f8cdcbb29f1ad30ffde2e46e5ae3ac
Mark Swardstrom <swards@gmail.com>
2023-09-10 11:28:44 -0700

    Adding volunteer mailer

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -3,6 +3,7 @@ class VolunteerMailer < ActionMailer::Base
   layout 'mailer'
 
   def welcome (volunteer)
+    @volunteer = volunteer
     mail(
       :from => "info@looseendsproject.org",
       :to => volunteer.user.email,
