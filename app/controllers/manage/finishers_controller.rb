737c2ad0a8a88534ae565336a93ea0e438336b90
Mark Swardstrom <swards@gmail.com>
2023-09-20 22:14:45 -0700

    SHowing titles for finisher and search

--- app/controllers/manage/finishers_controller.rb
+++ app/controllers/manage/finishers_controller.rb
@@ -8,10 +8,14 @@ class Manage::FinishersController < Manage::ManageController
     @states = @stateQuery.pluck(:state).uniq
     @existing_countries = Finisher.where.not(country: "").order(:country).load_async.pluck(:country).uniq
     @countries = ISO3166::Country.all.select{ |c| @existing_countries.include?(c.alpha2) }.map{ |c| [c.iso_short_name, c.alpha2]}.sort_by{|c| I18n.transliterate(c[0])}
+    skill = Skill.find(params[:skill_id]) if params[:skill_id].present?
+    product = Product.find(params[:product_id]) if params[:product_id].present?
+    @title = ['Finishers', params[:search], params[:country], skill&.name, product&.name].reject(&:blank?).join(' ')
   end
 
   def show
     @finisher = Finisher.find(params[:id])
+    @title = @finisher.chosen_name
   end
 
   def edit
