294017b07ffff66a5427289a5acb34a27f4f183f
ruscias <stevenwrusciano@gmail.com>
2023-10-21 23:30:38 -0500

    First pass at improving visualization of markers

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,9 +1,9 @@
 console.log("success");
 
 async function initMap() {
-  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
-  const { Marker, PinElement, AdvancedMarkerElement } =
-    await google.maps.importLibrary("marker");
+  const { Map } = await google.maps.importLibrary("maps");
+  const { Point } = await google.maps.importLibrary("core");
+  const { Marker } = await google.maps.importLibrary("marker");
 
   let map;
 
@@ -21,13 +21,37 @@ async function initMap() {
 
   const finishers = document.querySelectorAll("div.finisher-list-item");
 
-  finishers.forEach((finisher) => {
-    const marker = new google.maps.Marker({
+  const svgMarker = {
+    path: "M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z",
+    fillColor: "purple",
+    fillOpacity: 0.9,
+    strokeColor: "black",
+    strokeWeight: 1,
+    rotation: 0,
+    scale: 1.8,
+    labelOrigin: new google.maps.Point(8, 7),
+    anchor: new google.maps.Point(8, 0),
+  };
+
+  finishers.forEach((finisher, i) => {
+    const marker = new Marker({
       position: {
         lat: parseFloat(finisher.getAttribute("data-latitude")),
         lng: parseFloat(finisher.getAttribute("data-longitude")),
       },
       map: map,
+      title: finisher.getAttribute("data-name"),
+      icon: svgMarker,
+      label: {
+        text: `${i + 1}`,
+        fontSize: "18px",
+        fontWeight: "900",
+        color: "white",
+      },
+    });
+
+    marker.addListener("click", () => {
+      console.log("marker clicked");
     });
   });
 }
