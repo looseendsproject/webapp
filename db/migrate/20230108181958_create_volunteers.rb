939bc0b5400621ab6b0160d725d6d4c8e2ee5ce4
Siobhan Coady <siobhan.coady@gmail.com>
2024-04-01 21:14:29 -0230

    Restore volunteers in migration files before change to finishers

--- db/migrate/20230108181958_create_volunteers.rb
+++ db/migrate/20230108181958_create_volunteers.rb
@@ -1,6 +1,6 @@
-class CreateFinishers < ActiveRecord::Migration[7.0]
+class CreateVolunteers < ActiveRecord::Migration[7.0]
   def change
-    create_table :finishers do |t|
+    create_table :volunteers do |t|
 
       t.string :chosen_name
       t.string :pronouns
--- db/migrate/20230108184735_create_assessments.rb
+++ db/migrate/20230108184735_create_assessments.rb
@@ -2,7 +2,7 @@ class CreateAssessments < ActiveRecord::Migration[7.0]
   def change
     create_table :assessments do |t|
       t.references :skill
-      t.references :finisher
+      t.references :volunteer
       t.integer :rating, null: false
       t.text :description
       t.timestamps
