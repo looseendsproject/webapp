cc93193e6cf8966869747944480d24c747edb959
Matt Sanford <matt@mzsanford.com>
2025-01-30 11:23:56 -0800

    Fix "Uncaught TypeError" in the JS console.
    The status toggle controller JS file was using some of the Hotwire concepts (turbo:load) but was not defining a 'Controller', in the Stimulus sense. The way it was implemented caused it to try and load on every page, even when no status dropdown existed. I converted the code into a Stimulus controller and attached it to the appropriate page. If this logic is only used on a single form it might be easier to understand in the page itself instead of as a Controller.

--- app/javascript/controllers/toggle_status_controller.js
+++ app/javascript/controllers/toggle_status_controller.js
@@ -1,24 +1,35 @@
-document.addEventListener('turbo:load', function() {
-  const statusDropdown = document.getElementById('status-dropdown');
-  const readyStatusRow = document.getElementById('ready-status-row');
-  const inProcessStatusRow = document.getElementById('in-process-status-row');
+import { Controller } from "@hotwired/stimulus";
 
-  function toggleReadyStatus() {
-    if (statusDropdown.value === 'ready to match') {
-      inProcessStatusRow.style.display = 'none';
-      readyStatusRow.style.display = 'block';
-    } else if (statusDropdown.value === 'in process') {
-      readyStatusRow.style.display = 'none';
-      inProcessStatusRow.style.display = 'block';
-    } else {
-      readyStatusRow.style.display = 'none';
-      inProcessStatusRow.style.display = 'none';
-    }
+// Define the value of status we care to act on. Defined at the top for easy reference
+// and to catch any typos in the future.
+const statusValues = {
+  ready: 'ready to match',
+  inProcess: 'in process'
+};
+
+export default class extends Controller {
+  static targets = ["status", "readyStatus", "inProcessStatus"];
+
+  connect() {
+    // Apply current value on initial page load
+    this.applyStatus(this.statusTarget.value);
   }
 
-  // Check the initial status on page load
-  toggleReadyStatus();
-  
-  // Add event listener for changes in the status dropdown
-  statusDropdown.addEventListener('change', toggleReadyStatus);
-});
+  statusChanged(event) {
+    // Apply the new value when the select changes
+    this.applyStatus(event.target.value);
+  }
+
+  applyStatus(status) {
+    if (status === statusValues.ready) {
+      this.readyStatusTarget.style.display = 'block';
+      this.inProcessStatusTarget.style.display = 'none';
+    } else if (status === statusValues.inProcess) {
+      this.readyStatusTarget.style.display = 'none';
+      this.inProcessStatusTarget.style.display = 'block';
+    } else {
+      this.readyStatusTarget.style.display = 'none';
+      this.inProcessStatusTarget.style.display = 'none';
+    }
+  }
+}
