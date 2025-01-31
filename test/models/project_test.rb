f2e4b6a2925239c1fb8ea3893bc035efc6d8fcca
Matt Sanford <matt@mzsanford.com>
2025-01-31 11:12:00 -0800

    Add tests for a few of the helpers in the Project model

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -13,19 +13,52 @@ class ProjectTest < ActiveSupport::TestCase
 
   test 'Name update' do
     @project.name = 'Updated Name'
-    @project.save
+    @project.save!
     assert_equal('Updated Name', @project.reload.name)
   end
 
   test 'Valid phone number update' do
     @project.phone_number = '1231231234'
-    @project.save
+    @project.save!
     assert_equal('1231231234', @project.reload.phone_number)
   end
 
   test 'Short phone number not allowed' do
     @project.phone_number = '123123123'
-    @project.save
     refute(@project.valid?, 'Short phone number should not be allowed')
+  end
+
+  test 'status defaults to proposed if the project IS NOT missing information' do
+    @project.status = nil
+    @project.save!
+    assert_equal('proposed', @project.reload.status)
+  end
+
+  test 'status defaults to drafted if the project IS missing information' do
+    @project.has_pattern = nil
+    @project.status = nil
+    @project.save!
+    assert_equal('drafted', @project.reload.status)
+  end
+
+  test 'invalid status rejected' do
+    @project.status = 'invalid status'
+    refute(@project.valid?, 'Invalid status should not be allowed')
+  end
+
+  test 'missing_address_information? helper' do
+    refute(@project.missing_address_information?, 'Project fixture should not be missing address information')
+    [:street, :city, :state, :postal_code, :country].each do |field|
+      @project[field] = nil
+      assert(@project.missing_address_information?, "Project should be missing address information when #{field} is nil")
+    end
+  end
+
+  test 'missing_information? helper' do
+    refute(@project.missing_information?, 'Project fixture should not be missing information')
+    [:description, :phone_number, :has_pattern, :material_type].each do |field|
+      @project[field] = nil
+      assert(@project.missing_information?, "Project should be missing information when #{field} is nil")
+    end
   end
 end
