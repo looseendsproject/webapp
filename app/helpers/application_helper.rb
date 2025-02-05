ef9b35504a6afaba9ac3d3f786280059f1493cb8
Bob Gatewood <bgatewood@users.noreply.github.com>
2025-02-05 09:05:42 -0500

    Set up staging environment (2nd attempt) (#57)
    * Add dotenv and RAILS_DISPLAY_ENV
    * Incorporate feedback
    * Update app/helpers/application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def fa_icon icon_name, options = {}
     return content_tag(:i, nil, options.merge(:class => "fa fa-#{icon_name}"))
   end
+
+  # Prepends DEVELOPMENT, TEST, or STAGING to page.title
+  # null RAILS_DISPLAY_ENV or 'production' does not change title
+  def display_env
+    unless ENV['RAILS_DISPLAY_ENV'].blank? ||  Rails.env.production?
+      return "#{ENV['RAILS_DISPLAY_ENV'].upcase} "
+    end
+    ''
+  end
 end
