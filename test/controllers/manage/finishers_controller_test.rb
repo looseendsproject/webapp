3da152b5aa941034c56f90b34d279be94781a39d
Matt Sanford <matt@mzsanford.com>
2025-02-05 14:04:22 -0800

    Make finisher search test more robust to handle fixture change

--- test/controllers/manage/finishers_controller_test.rb
+++ test/controllers/manage/finishers_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class Manage::FinishersControllerTest < ActionController::TestCase
   setup do
@@ -6,12 +6,12 @@ class Manage::FinishersControllerTest < ActionController::TestCase
     assert(@user.can_manage?)
   end
 
-  test 'search requires login' do
+  test "search requires login" do
     get :search
     assert_redirected_to new_user_registration_path
   end
 
-  test 'search requires can_manage? access' do
+  test "search requires can_manage? access" do
     @user_without_manager = users(:basic)
     refute(@user_without_manager.can_manage?)
 
@@ -20,37 +20,40 @@ class Manage::FinishersControllerTest < ActionController::TestCase
     assert_redirected_to root_path
   end
 
-  test 'search with no parameters' do
+  test "search with no parameters" do
     sign_in @user
     get :search
     assert_response :success
     assert_equal([], JSON.parse(response.body))
   end
 
-  test 'search with no matches' do
+  test "search with no matches" do
     sign_in @user
-    get :search, params: { term: 'xyz' }
+    get :search, params: { term: "xyz" }
     assert_response :success
     assert_equal([], JSON.parse(response.body))
   end
 
-  test 'search with parameters returning one match' do
+  test "search with parameters returning one match" do
+    match_finisher = finishers(:knitter)
     sign_in @user
-    get :search, params: { term: 'fish' }
+    get :search, params: { term: "fran" }
     assert_response :success
-    assert_equal([{"id"=>2, "name"=>"Fran Fishman"}], JSON.parse(response.body))
+    assert_equal([{ "id" => match_finisher.id, "name" => match_finisher.chosen_name }], JSON.parse(response.body))
   end
 
-  test 'search with parameters returning multiple matches' do
+  test "search with parameters returning multiple matches" do
+    match_finishers = [finishers(:crocheter), finishers(:knitter)]
     sign_in @user
-    get :search, params: { term: 'f' }
+    get :search, params: { term: "y" }
     assert_response :success
-    assert_equal([{"id"=>1, "name"=>"Fae Fenwick"}, {"id"=>2, "name"=>"Fran Fishman"}], JSON.parse(response.body))
+    assert_equal(match_finishers.map { |f| { "id" => f.id, "name" => f.chosen_name } },
+                 JSON.parse(response.body))
   end
 
-  test 'does not allow SQL injection' do
+  test "does not allow SQL injection" do
     sign_in @user
-    get :search, params: { term: 'f;\' DROP TABLE finishers; --' }
+    get :search, params: { term: "f;' DROP TABLE finishers; --" }
     assert_response :success
     assert_equal([], JSON.parse(response.body))
     assert(Finisher.count > 0)
