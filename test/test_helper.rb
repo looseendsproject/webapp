733938ec00960b7c3deb4d6aad733329f1c7cdc0
Matt Sanford <matt@mzsanford.com>
2025-02-06 12:12:47 -0800

    De-parallelize unit tests (as Rails does not play nice) and make it so overcommit always provides the MacOS required env to prevent tests hanging

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,24 +3,21 @@ require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
-
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
-  Geocoder.configure(:lookup => :test)
+  Geocoder.configure(lookup: :test)
   # Locations form test fixtures
   Geocoder::Lookup::Test.add_stub(
-    "123 Main St, , Anytown, WA, 12345", [    {
-      'latitude'     => 40.7143528,
-      'longitude'    => -74.0059731,
-      'address'      => 'New York, NY, USA',
-      'state'        => 'New York',
-      'state_code'   => 'NY',
-      'country'      => 'United States',
-      'country_code' => 'US'
+    "123 Main St, , Anytown, WA, 12345", [{
+      "latitude" => 40.7143528,
+      "longitude" => -74.0059731,
+      "address" => "New York, NY, USA",
+      "state" => "New York",
+      "state_code" => "NY",
+      "country" => "United States",
+      "country_code" => "US"
     }]
   )
 end
