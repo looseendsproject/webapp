8c651ce9abf39ad5d8c641d6d65b16d62995b20f
Bob <bgatewood@gmail.com>
2025-02-06 09:03:43 -0500

    Fix test

--- test/controllers/manage/reports_controller_test.rb
+++ test/controllers/manage/reports_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class Manage::ReportsControllerTest < ActionController::TestCase
   test "heard_about_us" do
-    sign_in users(:anne)
+    sign_in users(:admin)
     get :heard_about_us
     assert_response :success
   end
