387d3c638f845f7e7212f338a4bd630447c11908
Matt Sanford <matt@mzsanford.com>
2025-02-11 09:58:40 -0800

    Fix bug when Finisher missing joined_on
    Signed-off-by: Matt Sanford <matt@mzsanford.com>

--- app/controllers/manage/finishers_controller.rb
+++ app/controllers/manage/finishers_controller.rb
@@ -1,29 +1,35 @@
 class Manage::FinishersController < Manage::ManageController
-
-  before_action :get_project, only: [:index, :map, :card]
-  require 'csv'
+  before_action :get_project, only: %i[index map card]
+  require "csv"
   def index
     respond_to do |format|
       skill = Skill.find(params[:skill_id]) if params[:skill_id].present?
       product = Product.find(params[:product_id]) if params[:product_id].present?
-      @title = ['Loose Ends - Manage - Finishers', params[:search].present? ? "'#{params[:search]}'" : nil, params[:country], params[:state], skill&.name, product&.name, params[:sort].present? ? "sort by #{params[:sort]}" : nil].reject(&:blank?).join(' ')
+      @title = ["Loose Ends - Manage - Finishers", params[:search].present? ? "'#{params[:search]}'" : nil,
+                params[:country], params[:state], skill&.name, product&.name, params[:sort].present? ? "sort by #{params[:sort]}" : nil].reject(&:blank?).join(" ")
       format.csv do
-        response.headers['Content-Type'] = 'text/csv'
-        response.headers['Content-Disposition'] = "attachment; filename=#{@title.parameterize}-#{DateTime.now.strftime("%Y-%m-%d-%H%M")}.csv"
-        @finishers = Finisher.includes(:user).select(:id, :user_id, :first_name, :last_name, :email, :has_workplace_match).search(params)
+        response.headers["Content-Type"] = "text/csv"
+        response.headers["Content-Disposition"] =
+          "attachment; filename=#{@title.parameterize}-#{DateTime.now.strftime("%Y-%m-%d-%H%M")}.csv"
+        @finishers = Finisher.includes(:user).select(:id, :user_id, :first_name, :last_name, :email,
+                                                     :has_workplace_match).search(params)
       end
       format.html do
-        first = Finisher.order(:joined_on).first.joined_on.beginning_of_month
+        first_finisher = Finisher.order(:joined_on).first
+        first = first_finisher.joined_on&.beginning_of_month || first_finisher.created_at.beginning_of_month
         last = Date.today.beginning_of_month
-        @months = (first..last).map{ |date| date.strftime("%Y-%m-01") }.uniq.reverse
-        @finishers = Finisher.includes(:products, :user, { :rated_assessments => :skill }).with_attached_picture.search(params).paginate(page: params[:page])
-        if params[:country].present?
-          @states = Finisher.where(country: params[:country]).distinct.pluck(:state).reject(&:blank?).sort
-        else
-          @states = Finisher.distinct.pluck(:state).reject(&:blank?).sort
-        end
+        @months = (first..last).map { |date| date.strftime("%Y-%m-01") }.uniq.reverse
+        @finishers = Finisher.includes(:products, :user,
+                                       { rated_assessments: :skill }).with_attached_picture.search(params).paginate(page: params[:page])
+        @states = if params[:country].present?
+                    Finisher.where(country: params[:country]).distinct.pluck(:state).reject(&:blank?).sort
+                  else
+                    Finisher.distinct.pluck(:state).reject(&:blank?).sort
+                  end
         @existing_countries = Finisher.distinct.pluck(:country).reject(&:blank?).sort
-        @countries = ISO3166::Country.all.select{ |c| @existing_countries.include?(c.alpha2) }.map{ |c| [c.iso_short_name, c.alpha2]}.sort_by{|c| I18n.transliterate(c[0])}
+        @countries = ISO3166::Country.all.select do |c|
+          @existing_countries.include?(c.alpha2)
+        end.map { |c| [c.iso_short_name, c.alpha2] }.sort_by { |c| I18n.transliterate(c[0]) }
       end
     end
   end
@@ -32,14 +38,14 @@ class Manage::FinishersController < Manage::ManageController
     @title = "Loose Ends - Manage - Finishers Map"
     params[:radius] ||= 50
     results = Geocoder.search(params[:near])
-    if results.first
-      @finishers = Finisher.geocoded.near(results.first.coordinates, params[:radius])
-      if params[:skill_id].present?
-        @finishers = @finishers.joins(:assessments).where(:assessments => { skill_id: params[:skill_id], rating: 1.. })
-        @skill_id = params[:skill_id]
-      end
-      @center = results.first.coordinates
+    return unless results.first
+
+    @finishers = Finisher.geocoded.near(results.first.coordinates, params[:radius])
+    if params[:skill_id].present?
+      @finishers = @finishers.joins(:assessments).where(assessments: { skill_id: params[:skill_id], rating: 1.. })
+      @skill_id = params[:skill_id]
     end
+    @center = results.first.coordinates
   end
 
   def show
@@ -71,21 +77,18 @@ class Manage::FinishersController < Manage::ManageController
   end
 
   def search
-    if params[:term].blank?
-      return render json: []
-    end
+    return render json: [] if params[:term].blank?
 
     # Fetch finishers matching the search term (case insensitive)
     finishers = Finisher
-                  .where('chosen_name ILIKE ?', "%#{params[:term]}%")
-                  .limit(20)
-                  .select(:id, :chosen_name)
+                .where("chosen_name ILIKE ?", "%#{params[:term]}%")
+                .limit(20)
+                .select(:id, :chosen_name)
 
     render json: finishers.map { |f| { id: f.id, name: f.chosen_name } }
   end
 
   protected
-
 
   def finisher_params
     params.require(:finisher).permit(:admin_notes,
@@ -112,9 +115,8 @@ class Manage::FinishersController < Manage::ManageController
   private
 
   def get_project
-    if params[:project_id]
-      @project = Project.find(params[:project_id])
-    end
-  end
+    return unless params[:project_id]
 
+    @project = Project.find(params[:project_id])
+  end
 end
