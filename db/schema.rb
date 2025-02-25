691930e49eeeef3d56922f276a7089e56469cc8a
Mark Swardstrom <swards@gmail.com>
2025-02-25 08:24:58 -0800

    Adding annotations

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -23,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_11_174223) do
     t.bigint "record_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index %w[record_type record_id name], name: "index_action_text_rich_texts_uniqueness", unique: true
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -33,8 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_11_174223) do
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index %w[record_type record_id name blob_id], name: "index_active_storage_attachments_uniqueness",
-                                                    unique: true
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
@@ -52,7 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_11_174223) do
   create_table "active_storage_variant_records", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
-    t.index %w[blob_id variation_digest], name: "index_active_storage_variant_records_uniqueness", unique: true
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "agencies", id: :serial, force: :cascade do |t|
@@ -362,7 +359,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_11_174223) do
     t.string "queue", limit: 255
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
-    t.index %w[priority run_at], name: "delayed_jobs_priority"
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
   create_table "ebp_documents", id: :serial, force: :cascade do |t|
@@ -543,7 +540,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_11_174223) do
     t.string "lon", limit: 255
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
-    t.index %w[lat lon], name: "index_locations_on_lat_and_lon"
+    t.index ["lat", "lon"], name: "index_locations_on_lat_and_lon"
   end
 
   create_table "lookups", id: :serial, force: :cascade do |t|
@@ -554,7 +551,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_11_174223) do
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
     t.index ["code"], name: "index_lookups_on_code"
-    t.index %w[type code], name: "index_lookups_on_type_and_code"
+    t.index ["type", "code"], name: "index_lookups_on_type_and_code"
   end
 
   create_table "measure_question_answers", id: :serial, force: :cascade do |t|
@@ -656,7 +653,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_11_174223) do
     t.text "body"
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
-    t.index %w[targetable_id targetable_type], name: "index_messages_on_targetable_id_and_targetable_type"
+    t.index ["targetable_id", "targetable_type"], name: "index_messages_on_targetable_id_and_targetable_type"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
@@ -678,7 +675,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_11_174223) do
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
     t.index ["code"], name: "index_organization_lookups_on_code"
-    t.index %w[type code], name: "index_organization_lookups_on_type_and_code"
+    t.index ["type", "code"], name: "index_organization_lookups_on_type_and_code"
   end
 
   create_table "organization_measures", id: :serial, force: :cascade do |t|
