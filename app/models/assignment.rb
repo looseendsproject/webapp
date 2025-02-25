bcbc25697def6d0ffa0b4cfeda66aa09352cd0c1
Matt Sanford <matt@mzsanford.com>
2025-02-25 12:14:34 -0800

    Assignment status blank handling
    Allow an empty string to be passed in as the Assignment#status. This is
    treated the same as nil. This is needed to accommodate any Assignment
    that existed prior to the introduction of the status field.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -30,7 +30,7 @@ class Assignment < ApplicationRecord
   has_many :assignment_updates, dependent: :destroy
 
   validates :finisher_id, uniqueness: { scope: :project_id }
-  validates :status, inclusion: { in: STATUS }, allow_nil: true
+  validates :status, inclusion: { in: STATUS, allow_blank: true }
 
   def self.active
     where(ended_at: nil)
--- test/models/assignment_test.rb
+++ test/models/assignment_test.rb
@@ -27,12 +27,14 @@ class AssignmentTest < ActiveSupport::TestCase
 
   test "all fixtures are valid by default" do
     Assignment.all.each do |assignment|
-      assert(assignment.valid?, "Assignment fixture should be valid: #{assignment.errors.full_messages.to_sentence}")
+      assert_predicate(assignment, :valid?,
+                       "Assignment fixture should be valid: #{assignment.errors.full_messages.to_sentence}")
     end
   end
 
   test "does not allow unknown status" do
     @assignment.status = "unknown"
+
     assert_not @assignment.valid?
     assert_includes @assignment.errors.full_messages, "Status is not included in the list"
   end
@@ -40,13 +42,21 @@ class AssignmentTest < ActiveSupport::TestCase
   test "allows known status" do
     Assignment::STATUS.each do |status|
       @assignment.status = status
-      assert @assignment.valid?, "Status #{status} should be valid"
+
+      assert_predicate @assignment, :valid?, "Status #{status} should be valid"
     end
   end
 
   test "allows nil status" do
     @assignment.status = nil
-    assert @assignment.valid?
+
+    assert_predicate @assignment, :valid?
+  end
+
+  test "allows empty string status" do
+    @assignment.status = ""
+
+    assert_predicate @assignment, :valid?
   end
 
   test "active scope" do
