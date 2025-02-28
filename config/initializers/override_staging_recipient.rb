5b12f0f3ba6739cb931115fc2da697e4b86bdf60
Bob Gatewood <bgatewood@users.noreply.github.com>
2025-02-28 14:56:39 -0500

    Fix staging mail interceptor (#144)
    Record original_recipient in the subject line

--- config/initializers/override_staging_recipient.rb
+++ config/initializers/override_staging_recipient.rb
@@ -3,11 +3,10 @@
 if ENV['RAILS_ENV_DISPLAY'] == 'staging'
   class OverrideStagingRecipient
     def self.delivering_email(mail)
-      original_reipient = mail.to
+      original_recipient = mail.to
       mail.to = 'app-staging@looseendsproject.org'
       mail.from = 'app-staging@looseendsproject.org'
-      mail.subject = "[#{ENV['RAILS_ENV_DISPLAY'].upcase}] #{mail.subject}"
-      mail.body = "[original recipient: #{original_reipient}]\n\n" + mail.body
+      mail.subject = "[#{ENV['RAILS_ENV_DISPLAY'].upcase}/#{original_recipient}] #{mail.subject}"
     end
   end
   ActionMailer::Base.register_interceptor(OverrideStagingRecipient)
