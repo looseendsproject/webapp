007338432d66da4e7c7ae14d8bb0a9920c89641e
Mercedes Bernard <mercedesrbernard@gmail.com>
2025-03-05 08:45:47 -0600

    remove default false for has_volunteer_time_off

--- db/schema.rb
+++ db/schema.rb
@@ -466,7 +466,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_27_211311) do
     t.string "emergency_contact_phone_number"
     t.string "emergency_contact_email"
     t.string "emergency_contact_relation"
-    t.boolean "has_volunteer_time_off", default: false
+    t.boolean "has_volunteer_time_off"
     t.index ["joined_on"], name: "index_finishers_on_joined_on"
     t.index ["latitude"], name: "index_finishers_on_latitude"
     t.index ["longitude"], name: "index_finishers_on_longitude"
