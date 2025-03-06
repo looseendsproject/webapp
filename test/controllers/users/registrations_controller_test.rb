e9a0bb5b0333e200252e92336889e3f656c85a29
Bob <bgatewood@gmail.com>
2025-03-06 14:13:42 -0500

    Conflict

--- test/controllers/users/registrations_controller_test.rb
+++ test/controllers/users/registrations_controller_test.rb
@@ -30,4 +30,19 @@ class Users::RegistrationsControllerTest < ActionController::TestCase
     assert_redirected_to :root
     assert User.last.first_name == 'Testy'
   end
+
+  test 'new registration sends confirmation instructions email' do
+    post :create, params: { user: {
+      first_name: 'First',
+      last_name: 'Last',
+      email: 'me@example.com',
+      heard_about_us: 'Blimp Ad',
+      password: 'not_a_good_password',
+      password_confirmation: 'not_a_good_password'
+    } }
+    assert_response :redirect
+    assert_equal User.last.email, 'me@example.com'
+    assert_equal "Confirmation instructions", ActionMailer::Base.deliveries.last.subject
+    assert_equal  ['me@example.com'], ActionMailer::Base.deliveries.last.to
+  end
 end
