5d4fc2709308bc0690715ec9e60223c65db5271c
Bob <bgatewood@gmail.com>
2025-03-06 14:02:34 -0500

    Conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -60,7 +60,7 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable, :trackable
 
   validates :role, inclusion: { in: ROLES }
--- db/schema.rb
+++ db/schema.rb
@@ -1144,6 +1144,10 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_11_174223) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_project_manager"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
