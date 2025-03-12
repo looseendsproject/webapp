1aed85f728f5023b7f27c67350065112ac1803a1
Matt Sanford <matt@mzsanford.com>
2025-03-12 09:41:22 -0700

    Add material_brand attribute to Project, make searchable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_250_308_235_102) do
+ActiveRecord::Schema[7.0].define(version: 2025_03_12_164001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
@@ -216,6 +216,7 @@ ActiveRecord::Schema[7.0].define(version: 20_250_308_235_102) do
     t.string "press_outlet"
     t.boolean "can_use_first_name", default: false
     t.boolean "can_share_crafter_details", default: false
+    t.text "material_brand"
     t.index ["group_manager_id"], name: "index_projects_on_group_manager_id"
     t.index ["latitude"], name: "index_projects_on_latitude"
     t.index ["longitude"], name: "index_projects_on_longitude"
