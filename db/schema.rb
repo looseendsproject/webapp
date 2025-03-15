662baa771a6be0ba0f5573b87dff698c629d9078
Bob <bgatewood@gmail.com>
2025-03-15 13:03:42 -0400

    More schema diffs

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema[8.0].define(version: 2025_03_12_164001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
-  enable_extension "pg_stat_statements"
 
   create_table "action_mailbox_inbound_emails", force: :cascade do |t|
     t.integer "status", default: 0, null: false
@@ -21,7 +20,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_12_164001) do
     t.string "message_checksum", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index %w[message_id message_checksum], name: "index_action_mailbox_inbound_emails_uniqueness", unique: true
+    t.index ["message_id", "message_checksum"], name: "index_action_mailbox_inbound_emails_uniqueness", unique: true
   end
 
   create_table "action_text_rich_texts", force: :cascade do |t|
@@ -31,7 +30,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_12_164001) do
     t.bigint "record_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index %w[record_type record_id name], name: "index_action_text_rich_texts_uniqueness", unique: true
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -41,8 +40,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_12_164001) do
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index %w[record_type record_id name blob_id], name: "index_active_storage_attachments_uniqueness",
-                                                    unique: true
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
@@ -60,7 +58,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_12_164001) do
   create_table "active_storage_variant_records", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
-    t.index %w[blob_id variation_digest], name: "index_active_storage_variant_records_uniqueness", unique: true
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "assessments", force: :cascade do |t|
