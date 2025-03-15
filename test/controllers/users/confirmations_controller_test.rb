1f9efc29494462ef34953046cde54b4e979a6b48
Bob <bgatewood@gmail.com>
2025-03-15 13:38:22 -0400

    Confirmation email test

--- test/controllers/users/confirmations_controller_test.rb
+++ test/controllers/users/confirmations_controller_test.rb
@@ -10,9 +10,10 @@ class Users::ConfirmationsControllerTest < ActionController::TestCase
     assert_match /Resend confirmation instructions/, @response.body
   end
 
-  # test '#create sends email with correct link' do
-  #   post :create, params: { user_email: users(:admin) }
-  #   assert_response :success
-  #   assert_match 'flurp', ActionMailer::Base.deliveries.last.body
-  # end
+  test '#create sends email with correct link' do
+    post :create, params: { user: { email: users(:admin).email }}
+    assert_redirected_to new_user_session_url
+    message = ActionMailer::Base.deliveries.last
+    assert_match "http://example.com/users/confirmation?confirmation_token=", ActionMailer::Base.deliveries.last.body.encoded
+  end
 end
