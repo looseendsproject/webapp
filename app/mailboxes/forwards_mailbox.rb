70c22c79eb9437f5348fe771b8f4f2fccab45458
Bob Gatewood <bgatewood@gmail.com>
2025-03-18 17:49:12 -0400

    Email fixture & mailbox WIP

--- app/mailboxes/forwards_mailbox.rb
+++ app/mailboxes/forwards_mailbox.rb
@@ -1,5 +1,22 @@
 class ForwardsMailbox < ApplicationMailbox
+  before_processing :find_project
+
   def process
     # TODO
+  end
+
+  private
+
+  def find_original_sender
+    # find original from in forwarded body
+    # find user
+  end
+
+  def find_project
+    # user/finisher/project
+  end
+
+  def record_forward
+    # create ProjectNote record
   end
 end
