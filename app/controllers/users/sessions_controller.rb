a7670234f2c7b68cc4a5dc86020cb77516b0d3e1
Bob Gatewood <bgatewood@gmail.com>
2025-04-04 15:31:47 -0400

    Refactor Users::SessionsController for readability

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,36 +2,51 @@
 
 module Users
   class SessionsController < Devise::SessionsController
+    before_action :ensure_params, only: [:magic_link]
 
+    # Sign in user and redirect
     # GET /magic_link?sgid=eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaEpJaDVuYVdRNkx5OXN...
+    #
     def magic_link
-      begin
-        params.require(:sgid)
-      rescue ActionController::ParameterMissing
-        redirect_to '/users/sign_in', flash: { error: "Bad link. Please sign in to proceed." }
-        return
+      attempt_locate || handle_not_located
+    end
+
+    private
+
+      def ensure_params
+        begin
+          params.require(:sgid)
+        rescue ActionController::ParameterMissing
+          redirect_to '/users/sign_in', flash: { error: "Bad link. Please sign in to proceed." }
+          return
+        end
       end
 
-      # Happy path.  Message found.  all good
+      # If the action needs to do anything before redirect
       #
-      message = GlobalID::Locator.locate_signed(params[:sgid])
-      if message.present? && message.user.is_a?(User)
-        message.increment!(:click_count)
-        sign_in(message.user)
-        redirect_to message.send_link_action! and return
+      def attempt_locate(&block)
+        message = GlobalID::Locator.locate_signed(params[:sgid])
+        if message.present? && message.user.is_a?(User)
+          message.increment!(:click_count)
+          sign_in(message.user)
+
+          yield if block_given?
+
+          redirect_to message.send_link_action! and return true
+        end
       end
 
       # Locator returned nil. Figure out if it's expired or malformed
       #
-      message = Message.find_by(sgid: params[:sgid])
+      def handle_not_located
+        message = Message.find_by(sgid: params[:sgid])
 
-      if message.present? && message.expired?
-        @replacement = message.send_replacement!
-        render
-      else
-        raise ActiveRecord::RecordNotFound
+        if message.present? && message.expired?
+          render
+        else
+          raise ActiveRecord::RecordNotFound
+        end
       end
-    end
 
   end
 end
