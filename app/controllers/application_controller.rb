192928e28da6fad36519ba238608b7b2e7ba1ddd
Bob Gatewood <bgatewood@gmail.com>
2025-04-09 19:07:58 -0400

    More idomatic after_sign_in_path_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,9 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource_or_scope)
-    stored_location_for(resource_or_scope) || root_path
+    stored_location_for(resource_or_scope) && return
+    return manage_dashboard_path if resource&.can_manage?
+    root_path
   end
 
   private
