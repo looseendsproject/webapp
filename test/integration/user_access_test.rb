6a64b5a8cedccfa08e81b241fd1021c61e10b6d7
Bob Gatewood <bgatewood@users.noreply.github.com>
2025-04-18 14:07:16 -0400

    Hotfix: PDF pattern files + login redirect (#340) (#341)
    Fix an issue with PDF pattern files. This change uses file.variable? to
    guard attempting to generate a variant URL. If the file cannot generate
    a variant (like a PDF) we show the file name instead.
    
    Also fixed here was an issue where logins with a return path set in the
    Devise cookie were throwing a 500. There was a logic flaw in "&& return"
    causing a nil return value. A unit test was added to confirm. The
    before_filter used to set return paths needed to be prepended to return
    before authentication in the test.
    
    Co-authored-by: Matt Sanford <matt@mzsanford.com>

--- test/integration/user_access_test.rb
+++ test/integration/user_access_test.rb
@@ -1,26 +1,48 @@
-require 'test_helper'
+require "test_helper"
 
 class UserAccessTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
-  test 'landing page loads' do
-    get '/'
+  test "landing page loads" do
+    get "/"
+
     assert_response :success
   end
 
-  test 'user/registration loads' do
-    get '/users/sign_up'
+  test "user/registration loads" do
+    get "/users/sign_up"
+
     assert_response :success
   end
 
-  test '/finisher for unauthed request redirects' do
-    get '/finisher'
+  test "/finisher for unauthed request redirects" do
+    get "/finisher"
+
     assert_redirected_to new_user_session_path
   end
 
   test "finisher can view profile" do
     sign_in users(:finisher)
-    get '/finisher'
+    get "/finisher"
+
     assert_response :success
+  end
+
+  test "Sign in redirect from project redirects back" do
+    project = projects(:one)
+    user = project.user
+    user.password = "password"
+    user.save!
+    get "/projects/#{project.id}"
+
+    assert_redirected_to new_user_session_path
+    follow_redirect!
+
+    assert_response :success
+    assert_select "h1", { text: "Sign in" }
+
+    post "/users/sign_in", params: { user: { email: user.email, password: "password" } }
+
+    assert_redirected_to "/projects/#{project.id}"
   end
 end
