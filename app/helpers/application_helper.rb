196f1790f77d05b1f9e6fc6720475e2914263900
kt <kab761@gmail.com>
2025-05-05 14:57:06 -0700

    Hide material image and pattern file uploads unless required

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,10 @@ module ApplicationHelper
     content_tag(:i, nil, options.merge(class: "fa fa-#{icon_name}"))
   end
 
+  def availability_options(saved_value)
+    options_for_select(['Yes', 'No', "I don't know"], { selected: saved_value || "I don't know" })
+  end
+
   # For prepending DEVELOPMENT, TEST, or STAGING to page.title & elsewhere
   # (remember:  staging environment is RAILS_ENV=production)
   def display_env
