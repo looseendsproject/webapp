79ed7fa50c9ba3ecd6728f73a1fa1bb501f62f67
Bob Gatewood <bgatewood@users.noreply.github.com>
2025-05-05 10:31:01 -0400

    Expose confirmed/unconfirmed email addresses (#373)
    * Sort messages created_at desc on project/show
    
    * Show email confirmation status, allow update on Finisher edit
    
    * Confirm email on magic link click
    
    * Remove deprecated constant
    
    * Change :compact time format (%e?  what's the point if it inserts a space?)
    
    * Rake task to copy email blobs for dev
    
    * Clean up message/show for emails
    
    * Standardize date display on dashboard

--- app/controllers/manage/reports_controller.rb
+++ app/controllers/manage/reports_controller.rb
@@ -1,5 +1,7 @@
 class Manage::ReportsController < Manage::ManageController
+
   # Add route for each report and link on dashboard
+  # @results is a 2-D array [[col1, col2, col3], [col1, col2, col3],...]
 
   def heard_about_us
     @description = 'Counts of user records by heard_about_us value'
