07750b67db03a1da0c9342858e32d0a25175ddec
Bob Gatewood <bgatewood@gmail.com>
2025-05-07 13:58:43 -0400

    Better naming on test

--- test/controllers/manage/reports_controller_test.rb
+++ test/controllers/manage/reports_controller_test.rb
@@ -1,17 +1,15 @@
 require 'test_helper'
 
 class Manage::ReportsControllerTest < ActionController::TestCase
-  test "heard_about_us" do
-
-    all_routes = [
+  test "endpoints" do
+    endpoints = [
       :heard_about_us, :active_projects_by_status,
       :new_projects_by_month, :new_finishers_by_month
     ]
-
     sign_in users(:admin)
 
-    all_routes.each do |route|
-      get route
+    endpoints.each do |endpoint|
+      get endpoint
       assert_response :success
     end
   end
