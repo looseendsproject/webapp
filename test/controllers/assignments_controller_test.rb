1d7c444c53cc56789d7bfcce0ac85def7b1f18f1
Matt Sanford <matt@mzsanford.com>
2025-06-13 07:31:04 -0700

    Cleanup dashbopard state styles; Add all notes view (#441)
    * Allow state-specific styles for sentiment/attention
    
    * Add test for new validation
    
    * Add paginated finisher notes view
    
    * Update controller test top use valid sentiment

--- test/controllers/assignments_controller_test.rb
+++ test/controllers/assignments_controller_test.rb
@@ -3,7 +3,6 @@
 require "test_helper"
 
 class AssignmentsControllerTest < ActionDispatch::IntegrationTest
-
   def setup
     @assignment = assignments(:knit_active)
     sign_in(@assignment.finisher.user)
@@ -11,6 +10,7 @@ class AssignmentsControllerTest < ActionDispatch::IntegrationTest
 
   test "form loads" do
     get "/assignment/#{@assignment.id}/check_in"
+
     assert_response :success
   end
 
@@ -18,7 +18,8 @@ class AssignmentsControllerTest < ActionDispatch::IntegrationTest
     post "/assignment/#{@assignment.id}/check_in", params: { note: {
       sentiment: "going_well",
       text: "here's a very nice note"
-    }}
+    } }
+
     assert_redirected_to "/thank_you"
   end
 
@@ -27,19 +28,22 @@ class AssignmentsControllerTest < ActionDispatch::IntegrationTest
     post "/assignment/#{@assignment.id}/check_in", params: { note: {
       sentiment: "going_well",
       text: "here's a very nice note"
-    }}
-    assert @assignment.reload.last_contacted_at > old_date
+    } }
+
+    assert_operator @assignment.reload.last_contacted_at, :>, old_date
   end
 
   test "negative sentiment sends manager email" do
     post "/assignment/#{@assignment.id}/check_in", params: { note: {
       sentiment: "need_help",
       text: "this thing is a mess"
-    }}
+    } }
+
     assert_redirected_to "/thank_you"
     deliver_enqueued_emails
+
     assert_equal @assignment.project.manager.email,
-      ActionMailer::Base.deliveries.last.to.first
+                 ActionMailer::Base.deliveries.last.to.first
     assert_equal "negative_sentiment", @assignment.project.needs_attention
   end
 
@@ -47,41 +51,50 @@ class AssignmentsControllerTest < ActionDispatch::IntegrationTest
     post "/assignment/#{@assignment.id}/check_in", params: { note: {
       sentiment: "completed",
       text: "all done"
-    }}
+    } }
+
     assert_redirected_to "/thank_you"
     deliver_enqueued_emails
+
     assert_equal @assignment.project.manager.email,
-      ActionMailer::Base.deliveries.last.to.first
+                 ActionMailer::Base.deliveries.last.to.first
     assert_equal "completed", @assignment.project.needs_attention
   end
 
   test "positive sentiment does not send manager email" do
     post "/assignment/#{@assignment.id}/check_in", params: { note: {
-      sentiment: "positive",
+      sentiment: "going_well",
       text: "chugging along"
-    }}
+    } }
+
     assert_redirected_to "/thank_you"
     deliver_enqueued_emails
+
     assert_nil ActionMailer::Base.deliveries.last
   end
 
   test "rando cannot create note" do
     sign_out(@assignment.finisher.user)
     get "/assignment/#{@assignment.id}/check_in"
+
     assert_redirected_to "/users/sign_in"
   end
 
   test "must have sentiment, text not required unless need_help" do
-    post "/assignment/#{@assignment.id}/check_in", params: { note: { sentiment: "going_well" }}
+    post "/assignment/#{@assignment.id}/check_in", params: { note: { sentiment: "going_well" } }
+
     assert_redirected_to "/thank_you"
 
-    post "/assignment/#{@assignment.id}/check_in", params: { note: {}}
+    post "/assignment/#{@assignment.id}/check_in", params: { note: {} }
+
     assert_response :bad_request
 
-    post "/assignment/#{@assignment.id}/check_in", params: { note: { text: "text" }}
+    post "/assignment/#{@assignment.id}/check_in", params: { note: { text: "text" } }
+
     assert_response :unprocessable_content
 
-    post "/assignment/#{@assignment.id}/check_in", params: { note: { sentiment: "need_help", text: "" }}
+    post "/assignment/#{@assignment.id}/check_in", params: { note: { sentiment: "need_help", text: "" } }
+
     assert_response :unprocessable_content
   end
 end
