a12f414cb9fa503f7ad4a2c0ce99c4a7737394c8
Matt Sanford <matt@mzsanford.com>
2025-11-03 14:51:15 -0800

    Add country count reports for projects and finishers (#532)
    * Create finisher and project country reports
    
    * Add CSV view option on country reports
    
    * Clarify cart titles
    
    ---------
    
    Co-authored-by: Robert R. Meyer <20780405+robert-r-meyer@users.noreply.github.com>

--- app/controllers/manage/reports_controller.rb
+++ app/controllers/manage/reports_controller.rb
@@ -39,6 +39,22 @@ class Manage::ReportsController < Manage::ManageController
       Time.zone.now - 12.months).group_by_month(:created_at).count
   end
 
+  def project_countries
+    counts = Project.group(:country).order('count_id DESC').count(:id)
+    respond_to do |format|
+      format.csv { render_csv(counts) }
+      format.json { render json: counts }
+    end
+  end
+
+  def finisher_countries
+    counts = Finisher.group(:country).order('count_id DESC').count(:id)
+    respond_to do |format|
+      format.csv { render_csv(counts) }
+      format.json { render json: counts }
+    end
+  end
+
   def project_counts
     @description = "Counts of Projects by Status"
     @columns = %w(status count)
@@ -50,5 +66,19 @@ class Manage::ReportsController < Manage::ManageController
     @results["TOTAL -- all statuses"] = Project.count
     @results["NOT DONE -- total less DONE"] = @results["TOTAL -- all statuses"] - Project.where(status: "DONE").count
     render 'show'
+  end
+
+  private
+
+  def render_csv(results)
+    csv_string = CSV.generate do |csv|
+      csv << ['Country', 'Count']
+      results.each do |label, value|
+        csv << [label, value]
+      end
+    end
+
+    response.headers["Content-Type"] = "text/csv"
+    render plain: csv_string
   end
 end
