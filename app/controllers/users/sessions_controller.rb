f1337506131b5fc27dd4ff94631d762ec8144133
Siobhan Coady <siobhan.coady@gmail.com>
2026-02-18 19:39:49 -0330

    Make check-in email magic link one-time link (#572)

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,9 @@
 
 module Users
   class SessionsController < Devise::SessionsController
+    # Let magic_link run even when already signed in so we can show "already been used"
+    # instead of Devise's "You are already signed in." redirect to root.
+    skip_before_action :require_no_authentication, only: [:magic_link]
     before_action :ensure_params, only: [:magic_link]
 
     # Sign in user and redirect
@@ -27,6 +30,17 @@ module Users
       def attempt_locate(&block)
         message = GlobalID::Locator.locate_signed(params[:sgid])
         if message.present? && message.user.is_a?(User)
+          unless message.valid_sgid?
+            # If the user is already signed in, show the "already used" message
+            # on a page they are allowed to visit (not the sign-in page, which
+            # Devise will immediately redirect away from with "You are already signed in.")
+            if user_signed_in?
+              redirect_to "/", flash: { alert: "This link has already been used." }
+            else
+              redirect_to "/users/sign_in", flash: { alert: "This link has already been used." }
+            end
+            return true
+          end
           message.increment!(:click_count)
 
           sign_in(message.user)
